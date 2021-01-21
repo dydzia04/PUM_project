39b916fb1362cafd94d7038dc29e14a849a8f590
Patryk Dynda <PDyndal@gmail.com>
2021-01-21 23:19:40 +0100

    Added tests for controllers

--- library-server/src/controllers/author/author.controller.spec.ts
+++ library-server/src/controllers/author/author.controller.spec.ts
@@ -1,5 +1,6 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { AuthorController } from './author.controller';
+import { AuthorModel } from '../../models/author.model';
 
 describe('AuthorController', () => {
   let controller: AuthorController;
@@ -14,5 +15,29 @@ describe('AuthorController', () => {
 
   it('should be defined', () => {
     expect(controller).toBeDefined();
+  });
+
+  it('should return a value', () => {
+    expect(controller.getAuthors()).toReturn();
+  });
+
+  it('should return a list of authors', () => {
+    expect(controller.getAuthors()).toBe(Array(AuthorModel));
+  });
+
+  it('should return a value', () => {
+    expect(controller.getAuthor(1)).toReturn();
+  });
+
+  it('should return an author', () => {
+    expect(controller.getAuthor(1)).toBe(AuthorModel);
+  });
+
+  it('should return a value', function () {
+    expect(controller.addAuthor(new AuthorModel())).toBe(AuthorModel);
+  });
+
+  it('should create an author', function () {
+    expect(controller.addAuthor(new AuthorModel())).toBe(AuthorModel);
   });
 });
--- library-server/src/controllers/book/book.controller.spec.ts
+++ library-server/src/controllers/book/book.controller.spec.ts
@@ -1,5 +1,6 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { BookController } from './book.controller';
+import { BookModel } from '../../models/book.model';
 
 describe('BookController', () => {
   let controller: BookController;
@@ -14,5 +15,29 @@ describe('BookController', () => {
 
   it('should be defined', () => {
     expect(controller).toBeDefined();
+  });
+
+  it('should return a value', () => {
+    expect(controller.getBooks()).toReturn();
+  });
+
+  it('should return list of books', () => {
+    expect(controller.getBooks()).toBe(Array(BookModel));
+  });
+
+  it('should return a value', () => {
+    expect(controller.getBook(1)).toReturn();
+  });
+
+  it('should return a book', () => {
+    expect(controller.getBook(1)).toBe(BookModel);
+  });
+
+  it('should create a book', () => {
+    expect(controller.addBook(new BookModel())).toReturn();
+  });
+
+  it('should return a book', () => {
+    expect(controller.addBook(new BookModel())).toBe(BookModel);
   });
 });
