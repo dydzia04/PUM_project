80b80c8f3903891c9f6bb0821b34424377087508
Patryk Dynda <PDyndal@gmail.com>
2021-01-26 01:08:53 +0100

    Added DTO, updated titles, created api service. TODO add user service

--- library-ionic/src/app/app.module.ts
+++ library-ionic/src/app/app.module.ts
@@ -9,11 +9,18 @@ import { StatusBar } from '@ionic-native/status-bar/ngx';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import {HttpClientModule} from '@angular/common/http';
 
 @NgModule({
   declarations: [AppComponent],
   entryComponents: [],
-  imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule, FontAwesomeModule],
+  imports: [
+    BrowserModule,
+    IonicModule.forRoot(),
+    AppRoutingModule,
+    FontAwesomeModule,
+    HttpClientModule
+  ],
   providers: [
     StatusBar,
     SplashScreen,
